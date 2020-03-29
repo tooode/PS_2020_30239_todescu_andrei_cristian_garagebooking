71375f2e2bac56b6ed951b42e60cd4b1342a8a71
Andrei Cristian Todescu <andrei.todescu@yahoo.com>
2020-03-29 23:36:44 +0300

    UpdateTema1

--- Garage/src/main/java/repository/CarRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Integer> {
-}
--- Garage/src/main/java/repository/MechanicRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.Mechanic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MechanicRepository  extends JpaRepository<Mechanic, Integer> {
-}
