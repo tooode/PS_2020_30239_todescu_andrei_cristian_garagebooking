5ab96a8d9bab946fcf85123a663e8d159707cbe4
Andrei Cristian Todescu <andrei.todescu@yahoo.com>
2020-04-26 23:44:03 +0300

    Tema3 Factory Pattern

---
+++ Garage/src/main/java/architecture/Factory.java
@@ -0,0 +1,25 @@
+package architecture;
+
+import model.User;
+
+public class Factory {
+    public User createAccount(int id, String nume, boolean platitReparatie, int suma, String phoneNumber, String accountType){
+        User account = null;
+        if(accountType != null){
+            if(accountType.equals("Client")){
+                account = new User(id,nume,platitReparatie,suma,phoneNumber,"Client");
+            }
+            if(accountType.equals("Admin")) {
+                account = new User(id,nume,platitReparatie,suma,phoneNumber,"Admin");
+            }
+
+            if(account == null){
+                System.out.println("Users can be 2 types: Admin/Client!");
+            }
+        } else {
+            System.out.println("Users can be 2 types: Admin/Client!");
+        }
+
+        return account;
+    }
+}
